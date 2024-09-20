545cfd0e4c0fcc2043932713948a7527c5995607
Nicolas <nicolasbati8@gmail.com>
2024-09-20 12:26:46 -0300

    Modificando el app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
 
+let server;
+
+beforeAll(() => {
+  server = app.listen(4000);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
 describe('GET /', () => {
   it('should return Hello World', async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hello World!');
   });
